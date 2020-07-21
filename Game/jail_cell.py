c964375546aaa88b6b9580b4c7bb461f0cf94dcb
BuddyLoong <buddhaloong@hotmail.com>
2020-07-21 23:01:39 +0100

    Fixed Jail Cell

--- Game/jail_cell.py
+++ Game/jail_cell.py
@@ -5,19 +5,39 @@ Created on Tue Jul 21 17:28:38 2020
 @author: buddh
 """
 area = "Jail Cell"
-inventory = {"Cell_key":True,"Torch":False,"Door_key":False,"Sword":False}
-while (area == "Jail Cell"):
-    print ("What do you want to do?\n1.Look around.\n2.Wait for A while.")
+inventory = {"Cell_key":False,"Torch":False,"Door_key":False,"Sword":False}
+
+action = str()
+
+while (action == 2 or action == ""):
+    action = input("What do you want to do?\t1.Look around.\t2.Wait for A while")
+    action = int(action)
+    if (action == 2):
+        print ("You wait in your cell for a while nothing has changed.")
+    elif (action == 1):
+        print ("You look around.")
+        break 
+    else:
+        print ("This is not an option")
+print ("Got out of the first while loop.")
+        
+while (area == "Jail Cell" and action == 1):
+    print ("Looking around you can see:\t3.Locked door\t4.Rubble in a corner\t5.Barred off window.")
+
     user_input = input()
     user_input = int(user_input)
-    if (user_input == 1):
-        print ("Looking around you can see:\n3.Locked door\n4.Rubble in a corner\n5.Barred off window.")
-    elif (user_input == 2):
-        print ("You wait in your cell for a while nothing has changed.")
-        print ("What do you want to do?\n1.Look around.\n2.Wait for A while.")
-    elif (user_input == 3 and inventory["Cell_Key"] == True):
+    if (user_input == 3 and inventory["Cell_key"] == True):
         print ("The key fits perfectly and the door opens")
-    elif (user_input == 3 and inventory["Cell_Key"] == False):
-        print ("The door is locked. There is maybe a key around."
+        area = "Hallway"
+    elif (user_input == 3 and inventory["Cell_key"] == False):
+        print ("The door is locked.")
+    elif (user_input == 4 and inventory["Cell_key"] == False):
+        print("While searching through the rubble you find a rusty key.")
+        inventory["Cell_key"] = True
+    elif (user_input == 4 and inventory["Cell_key"] == True):
+        print ("You have already found the key.")
+    elif (user_input == 5):
+        print ("You can see outside through the window. Sadly the bars are stopping you from going through.")
+        
     else:
         print ("This is not an option")
