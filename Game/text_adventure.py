93d4fa0ab3a6c0387240f30591e8e0240ac12c23
BuddyLoong <buddhaloong@hotmail.com>
2020-07-22 21:05:23 +0100

    Title+Rooms+Comments

--- Game/text_adventure.py
+++ Game/text_adventure.py
@@ -7,9 +7,14 @@ Created on Mon Jul 20 23:10:08 2020
 #This is the print statment for the title screen
 print("Text Adventure")
 print("Options:\n1.Start\n2.Option\n3.Exit")
+#user_option is a variable that stores.
+#input is the user's input
+#int and str are used to convert a string or intreger.
 user_option = input("What do you want to do?1\\2\\3\n")
 user_option = int(user_option)
 #if depending on user input
+#if, elif and else are used if there is different user's input choice.
+#Different input will print out different statements
 if (user_option == 1):
     print("You wake up in a dark cell.")
 elif (user_option == 2):
