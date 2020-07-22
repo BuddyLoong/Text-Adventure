3cf81d889be03783b88e6882c5a34f9f7d641736
tienfongtang <tienftang@gmail.com>
2020-07-22 20:57:48 +0100

    Created a function in jail_cell.py. Linked jail_cell to text_adventure.py

--- Game/jail_cell.py
+++ Game/jail_cell.py
@@ -4,40 +4,42 @@ Created on Tue Jul 21 17:28:38 2020
 
 @author: buddh
 """
-area = "Jail Cell"
-inventory = {"Cell_key":False,"Torch":False,"Door_key":False,"Sword":False}
 
-action = str()
+def start_game():
+    area = "Jail Cell"
+    inventory = {"Cell_key":False,"Torch":False,"Door_key":False,"Sword":False}
 
-while (action == 2 or action == ""):
-    action = input("What do you want to do?\t1.Look around.\t2.Wait for A while")
-    action = int(action)
-    if (action == 2):
-        print ("You wait in your cell for a while nothing has changed.")
-    elif (action == 1):
-        print ("You look around.")
-        break 
-    else:
-        print ("This is not an option")
-print ("Got out of the first while loop.")
-        
-while (area == "Jail Cell" and action == 1):
-    print ("Looking around you can see:\t3.Locked door\t4.Rubble in a corner\t5.Barred off window.")
-
-    user_input = input()
-    user_input = int(user_input)
-    if (user_input == 3 and inventory["Cell_key"] == True):
-        print ("The key fits perfectly and the door opens")
-        area = "Hallway"
-    elif (user_input == 3 and inventory["Cell_key"] == False):
-        print ("The door is locked.")
-    elif (user_input == 4 and inventory["Cell_key"] == False):
-        print("While searching through the rubble you find a rusty key.")
-        inventory["Cell_key"] = True
-    elif (user_input == 4 and inventory["Cell_key"] == True):
-        print ("You have already found the key.")
-    elif (user_input == 5):
-        print ("You can see outside through the window. Sadly the bars are stopping you from going through.")
-        
-    else:
-        print ("This is not an option")
+    action = str()
+    
+    while (action == 2 or action == ""):
+        action = input("What do you want to do?\t1.Look around.\t2.Wait for A while")
+        action = int(action)
+        if (action == 2):
+            print ("You wait in your cell for a while nothing has changed.")
+        elif (action == 1):
+            print ("You look around.")
+            break 
+        else:
+            print ("This is not an option")
+    print ("Got out of the first while loop.")
+            
+    while (area == "Jail Cell" and action == 1):
+        print ("Looking around you can see:\t3.Locked door\t4.Rubble in a corner\t5.Barred off window.")
+    
+        user_input = input()
+        user_input = int(user_input)
+        if (user_input == 3 and inventory["Cell_key"] == True):
+            print ("The key fits perfectly and the door opens")
+            area = "Hallway"
+        elif (user_input == 3 and inventory["Cell_key"] == False):
+            print ("The door is locked.")
+        elif (user_input == 4 and inventory["Cell_key"] == False):
+            print("While searching through the rubble you find a rusty key.")
+            inventory["Cell_key"] = True
+        elif (user_input == 4 and inventory["Cell_key"] == True):
+            print ("You have already found the key.")
+        elif (user_input == 5):
+            print ("You can see outside through the window. Sadly the bars are stopping you from going through.")
+            
+        else:
+            print ("This is not an option")
--- Game/text_adventure.py
+++ Game/text_adventure.py
@@ -1,17 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 20 23:10:08 2020
+import jail_cell
 
-@author: buddh
-"""
 #This is the print statment for the title screen
 print("Text Adventure")
 print("Options:\n1.Start\n2.Option\n3.Exit")
-user_option = input("What do you want to do?1\\2\\3\n")
+user_option = input("What do you want to do?\t(1\\2\\3)\n")
 user_option = int(user_option)
 #if depending on user input
 if (user_option == 1):
     print("You wake up in a dark cell.")
+    jail_cell.start_game()
 elif (user_option == 2):
     print("1.Text Colour")
     print("2.Text Size")
@@ -19,4 +16,4 @@ elif (user_option == 2):
 elif (user_option == 3):
     print("Exit Game")
 else:
-    print("This is not an Option?")
+    print("This is not an Option")
