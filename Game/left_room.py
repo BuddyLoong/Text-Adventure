4d6b8e4394bfec7ed6fb62b9406981d63dcebd05
BuddyLoong <buddhaloong@hotmail.com>
2020-07-23 00:09:28 +0100

    4 Rooms

--- Game/left_room.py
+++ Game/left_room.py
@@ -1,23 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 22 18:03:48 2020
-
-@author: buddh
-"""
-
-area = "Left Room"
-inventory = {"Cell_key":True,"Torch":True,"Door_key":False,"Sword":False}
-
-action = str()
-
-while (action == 1 or action == ""):
-    action = input("What do you want to do?\t1.Look around.\t2.Go back")
-    action = int(action)
-    if (action == 2):
-        print ("You are now back in the hallway.")
-    elif (action == 1):
-        print ("You look around. There is nothing here except empty shelves.")
-        area = ("Hallway")
-        print (area)
-    else:
-        print ("This is not an option")
+import hallway
+def left_room_entry():
+    area = "Left Room"
+    inventory = {"Cell_key":True,"Torch":True,"Door_key":False,"Sword":False}
+    action = str()
+    
+    while (action == 1 or action == ""):
+        action = input("What do you want to do?\t1.Look around.\t2.Go back")
+        action = int(action)
+        if (action == 2):
+            print ("You are now back in the hallway.")
+            area = ("Hallway")
+            hallway.hallway_options()
+            print (area)
+        elif (action == 1):
+            print ("You look around. There is nothing here except empty shelves.")
+        else:
+            print ("This is not an option")
--- Game/right_room.py
+++ Game/right_room.py
@@ -1,26 +1,29 @@
-area ="Right room"
-inventory = {"Cell_key":True,"Torch":True,"Door_key":False,"Sword":False}
-
-print("1.Skeleton")
-print("2.Armour Stand")
-print("3.Weapon rack")
-print("4.Go back")
-
-while (area == "Right room"):
-    print (area)
-    user_input = input("What do you want to do?")
-    user_input = int(user_input)
-    if (user_input == 1):
-        print ("Checking the body reaveals a key.")
-        inventory["Door_key"] = True
-    elif (user_input == 2):
-        print ("You come across a rusty set of armour. It hold no value")
-    elif (user_input == 3):
-        print ("You see a rack with just one sword that still is sharp.")
-        inventory["Sword"] = True
-    elif (user_input == 4):
-        print ("You are now back in the hallway.")
-        area = "Hallway"
+import hallway
+def right_room_entry():
+    area ="Right room"
+    inventory = {"Cell_key":True,"Torch":True,"Door_key":False,"Sword":False}
+    
+    print("1.Skeleton")
+    print("2.Armour Stand")
+    print("3.Weapon rack")
+    print("4.Go back")
+    
+    while (area == "Right room"):
         print (area)
-    else:
-        print ("This is not an option.")
+        user_input = input("What do you want to do?")
+        user_input = int(user_input)
+        if (user_input == 1):
+            print ("Checking the body reaveals a key.")
+            inventory["Door_key"] = True
+        elif (user_input == 2):
+            print ("You come across a rusty set of armour. It hold no value")
+        elif (user_input == 3):
+            print ("You see a rack with just one sword that still is sharp.")
+            inventory["Sword"] = True
+        elif (user_input == 4):
+            print ("You are now back in the hallway.")
+            area = "End of Hallway"
+            print (area)
+            hallway.hallway_options()
+        else:
+            print ("This is not an option.")
